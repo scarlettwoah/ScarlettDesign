ea3500f01383af3027674a6a68078efbb9978e22
eugene <jeka.tomchuk@gmail.com>
2023-03-15 15:00:48 +0200

    add header.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+//MENU ICON IN HEADER
+
+const toggleBtn = document.querySelector(".header__toggle-btn");
+const toggleBtnIcon = document.querySelector(".header__toggle-btn i");
+const dropDownMenu = document.querySelector(".header__dropdown-menu");
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle("open");
+
+  const isOpen = dropDownMenu.classList.contains("open");
+  toggleBtnIcon.classList = isOpen
+    ? "fa-solid fa-xmark menu-icon"
+    : "fa-solid fa-bars menu-icon";
+};
+
+// ----------------------------------------------------------
